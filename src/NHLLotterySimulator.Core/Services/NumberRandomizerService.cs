c02c2cb3f28f3a54c45840b903a81e919fcebe58
Vincent Cadieux <vincent_cadieux@outlook.com>
2023-05-26 16:02:18 -0400

    Remove unnecessary memory allocation

--- src/NHLLotterySimulator.Core/Services/NumberRandomizerService.cs
+++ src/NHLLotterySimulator.Core/Services/NumberRandomizerService.cs
@@ -86,9 +86,7 @@ public class NumberRandomizerService
         while (n > 1)
         {
             int k = random.Next(n--);
-            T temp = array[n];
-            array[n] = array[k];
-            array[k] = temp;
+            (array[k], array[n]) = (array[n], array[k]);
         }
     }
 }
